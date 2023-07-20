611fa4b7cbe0ca698c24b5d85938c2b5652ae2cc
Fattan Haydar Ahmad <fattanhay0@gmail.com>
2023-07-20 21:14:28 +0700

    update

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -6,8 +6,22 @@ use Illuminate\Http\Request;
 
 use App\Http\Resources\NewsResource;
 
+use App\Models\News;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+
 class NewsController extends Controller
 {
+
+    public function show(News $news)
+  {
+    $news->load('comments'); // Load related comments for the news
+    return new NewsResource($news);
+  }
+
 
 public function index()
 {
@@ -18,12 +32,32 @@ public function index()
 
 public function store(Request $request)
 {
+
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|string|max:255',
+        'content' => 'required|string',
+        'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json(['error' => $validator->errors()], 400);
+    }
+
+    $user = Auth::user();
+
+    $news = new News();
+    $news->title = $request->input('title');
+    $news->content = $request->input('content');
+    $news->user_id = $user->id; 
+    $news->save();
+
     if ($request->hasFile('image')) {
         $imagePath = $request->file('image')->store('news_images', 'public');
         $news->image_url = $imagePath;
         $news->save();
     }
 
+    return new NewsResource($news);
 }
 
 public function update(Request $request, News $news)
@@ -39,8 +73,38 @@ public function update(Request $request, News $news)
 
 public function destroy(News $news)
 {
-    // Implement deletion of news
-    // Also, use event & listener to create a log for news deletion
-}
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|string|max:255',
+        'content' => 'required|string',
+        'image' => 'sometimes|image|mimes:jpeg,png,jpg,gif|max:2048',
+    ]);
 
+    if ($validator->fails()) {
+        return response()->json(['error' => $validator->errors()], 400);
+    }
+
+    $user = Auth::user();
+
+    if ($user->id !== $news->user_id) {
+        return response()->json(['error' => 'You are not authorized to update this news'], 403);
+    }
+
+    $news->title = $request->input('title');
+    $news->content = $request->input('content');
+    $news->save();
+
+
+    // Menghapus gambar terkait sebelum menghapus berita
+    if ($news->image_url) {
+        Storage::disk('public')->delete($news->image_url);
+    }
+
+    // Menghapus berita dari database
+    $news->delete();
+
+    return new NewsResource($news);
+
+    // Use event & listener to create a log for news deletion (belum diimplementasi)
+    // ...
+}
 }
